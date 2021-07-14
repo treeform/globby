63c1f0ce54f5ca814995baedf542685165a77116
Ryan Oldenburg <ryan@guzba.com>
2021-07-13 21:53:16 -0500

    rename

--- tests/benchmark.nim
+++ tests/benchmark.nim
@@ -2,20 +2,20 @@ import benchy, globby, random
 
 randomize()
 
-proc maybeAddPart(path: var string) =
+proc addPart(path: var string) =
   for i in 0 ..< rand(1..10):
     let c = rand(0..25) + 97 # ASCII
     path.add(c.char)
   path.add("/")
   if rand(1..100) <= 90:
-    maybeAddPart(path)
+    addPart(path)
   else:
     path.setLen(path.high)
 
 var paths = newSeq[string]()
 for i in 0 ..< 1000:
   var path = ""
-  maybeAddPart(path)
+  addPart(path)
   paths.add(path)
 
 let tree = GlobTree[int]()
